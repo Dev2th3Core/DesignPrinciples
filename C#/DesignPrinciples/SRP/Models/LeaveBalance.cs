c6d918c640bc206d652bf3460723e503b28b4a7b
Rakshit4045 <rakshitshinde150@gmail.com>
2025-07-02 19:58:05 +0530

    Leave support for Contract Employee in SRP

--- C#/DesignPrinciples/SRP/Models/LeaveBalance.cs
+++ C#/DesignPrinciples/SRP/Models/LeaveBalance.cs
@@ -5,6 +5,7 @@ namespace SRP.Models
     public class LeaveBalance
     {
         public int PaidLeaveRemaining { get; private set; } = 15;
+        public int UnPaidLeaveRemaining { get; private set;} = 60;
         public int CasualLeaveRemaining { get; private set; } = 10;
         public int SickLeaveRemaining { get; private set; } = 5;
 
@@ -16,6 +17,9 @@ namespace SRP.Models
             {
                 case LeaveType.Paid when PaidLeaveRemaining >= days:
                     PaidLeaveRemaining -= days;
+                    return true;
+                case LeaveType.Paid when UnPaidLeaveRemaining >= days:
+                    UnPaidLeaveRemaining -= days;
                     return true;
                 case LeaveType.Casual when CasualLeaveRemaining >= days:
                     CasualLeaveRemaining -= days;
@@ -36,6 +40,9 @@ namespace SRP.Models
             {
                 case LeaveType.Paid:
                     PaidLeaveRemaining += days;
+                    break;
+                case LeaveType.Unpaid:
+                    UnPaidLeaveRemaining += days;
                     break;
                 case LeaveType.Casual:
                     CasualLeaveRemaining += days;
